0821515905ca318e799dd3537fa5a78811081dc4
fabien <fabien.huet@gmail.com>
2018-09-12 13:15:54 +0700

    Init secrets.js file (not commited, see readme)

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,11 +1,6 @@
 /**
  * Switch script origin depending on dev / prod env
  */
-export interface IExtendedGlobal {
-  Bolt: object;
-  IS_DEV: boolean;
-}
-declare var global: IExtendedGlobal;
 global.Bolt = global.IS_DEV
   ? require('../dist_temp/index.js').default
   : require('../dist/index.js').default;
